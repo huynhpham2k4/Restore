0133e683610a8e2e97931616549791b700fb2ede
phamngochuynh2k4@gmai.com <phamngochuynh2k4@gmail.com>
2025-02-11 19:50:19 +0700

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,14 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 
 app.MapControllers();
 
