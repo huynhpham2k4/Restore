fc4d2fbe20aaf6b508bec303d97a4a0fee277b66
phamngochuynh2k4@gmai.com <phamngochuynh2k4@gmail.com>
2025-03-29 20:59:04 +0700

    End of section 11

--- client/src/app/layout/UserMenu.tsx
+++ client/src/app/layout/UserMenu.tsx
@@ -1,8 +1,9 @@
-import { Button, Menu, Fade, MenuItem, ListItemIcon, ListItemText, Divider } from "@mui/material";
+import { Button, Menu, Fade, ListItemIcon, ListItemText, Divider, MenuItem } from "@mui/material";
 import { useState } from "react";
 import { History, Logout, Person } from "@mui/icons-material";
 import { useLogoutMutation } from "../../features/account/accountApi";
-import { User } from "../models/user";
+import { User } from "../models/user"
+import { Link } from "react-router-dom";
 
 type Props = {
     user: User
@@ -25,7 +26,7 @@ export default function UserMenu({ user }: Props) {
                 onClick={handleClick}
                 color='inherit'
                 size='large'
-                sx={{fontSize: '1.1rem'}}
+                sx={{ fontSize: '1.1rem' }}
             >
                 {user.email}
             </Button>
@@ -45,7 +46,7 @@ export default function UserMenu({ user }: Props) {
                     </ListItemIcon>
                     <ListItemText>My profile</ListItemText>
                 </MenuItem>
-                <MenuItem>
+                <MenuItem component={Link} to='/orders'> 
                     <ListItemIcon>
                         <History />
                     </ListItemIcon>
