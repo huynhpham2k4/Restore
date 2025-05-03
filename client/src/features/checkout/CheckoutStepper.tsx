b09a98d52cbc1559cfeedeca1a1bab5286dd5fdb
phamngochuynh2k4@gmai.com <phamngochuynh2k4@gmail.com>
2025-05-03 11:45:45 +0700

    End of section 12_3

--- client/src/features/checkout/CheckoutStepper.tsx
+++ client/src/features/checkout/CheckoutStepper.tsx
@@ -3,7 +3,6 @@ import { AddressElement, PaymentElement, useElements, useStripe } from "@stripe/
 import { useState } from "react"
 import Review from "./Review";
 import { useFetchAddressQuery, useUpdateUserAddressMutation } from "../account/accountApi";
-import { Address } from "../../app/models/user";
 import { ConfirmationToken, StripeAddressElementChangeEvent, StripePaymentElementChangeEvent } from "@stripe/stripe-js";
 import { useBasket } from "../../lib/hooks/useBasket";
 import { currencyFormat } from "../../lib/util";
@@ -18,7 +17,8 @@ export default function CheckoutStepper() {
     const [activeStep, setActiveStep] = useState(0);
     const [createOrder] = useCreateOrderMutation();
     const { basket } = useBasket();
-    const { data: { name, ...restAddress } = {} as Address, isLoading } = useFetchAddressQuery();
+    // const { data: { name, ...restAddress } = {} as Address, isLoading } = useFetchAddressQuery();
+    const { data, isLoading } = useFetchAddressQuery();
     const [updateAddress] = useUpdateUserAddressMutation();
     const [saveAddressChecked, setSaveAddressChecked] = useState(false);
     const elements = useElements();
@@ -29,6 +29,11 @@ export default function CheckoutStepper() {
     const { total, clearBasket } = useBasket();
     const navigate = useNavigate();
     const [confirmationToken, setConfirmationToken] = useState<ConfirmationToken | null>(null);
+
+    let name, restAddress;
+    if (data) {
+        ({ name, ...restAddress } = data);
+    }
 
     const handleNext = async () => {
         if (activeStep === 0 && saveAddressChecked && elements) {
@@ -48,7 +53,7 @@ export default function CheckoutStepper() {
             await confirmPayment();
         }
         if (activeStep < 2) setActiveStep(step => step + 1);
-    } 
+    }
 
     const confirmPayment = async () => {
         setSubmitting(true);
@@ -68,7 +73,7 @@ export default function CheckoutStepper() {
             });
 
             if (paymentResult?.paymentIntent?.status === 'succeeded') {
-                navigate('/checkout/success', {state: orderResult});
+                navigate('/checkout/success', { state: orderResult });
                 clearBasket();
             } else if (paymentResult?.error) {
                 throw new Error(paymentResult.error.message);
@@ -85,7 +90,7 @@ export default function CheckoutStepper() {
         }
     }
 
-    const createOrderModel = async () => {  
+    const createOrderModel = async () => {
         const shippingAddress = await getStripeAddress();
         const paymentSummary = confirmationToken?.payment_method_preview.card;
 
